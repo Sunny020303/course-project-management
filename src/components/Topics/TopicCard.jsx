c4c428e577d8fd00c68aa0fc1dbd0974e1e2f07a
tdyhoang <tdyhoang@gmail.com>
2025-01-25 15:22:20 +0700

    fix

--- src/components/Topics/TopicCard.jsx
+++ src/components/Topics/TopicCard.jsx
@@ -393,7 +393,7 @@ const TopicCard = React.memo(function TopicCard({
                     )}
                   </MenuItem>
                   <MenuItem
-                    onClick={() => handfleDeleteTopic(selectedTopic.id)}
+                    onClick={() => handleDeleteTopic(selectedTopic.id)}
                     disabled={deletingTopic === selectedTopic.id}
                   >
                     {deletingTopic === selectedTopic.id ? (
